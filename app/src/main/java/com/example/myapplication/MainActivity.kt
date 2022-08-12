855a17e23dd6f1e095ae56f3552a1c2ba366291a
a.chumak <a.chumak@andersenlab.com>
2022-08-12 12:53:04 +0300

    add

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.myapplication
 
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import com.example.myapplication.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,9 +22,28 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val request = DownloadManager.Request(Uri.parse("https://www.dropbox.com/s/v457fzh5xyihrys/Terms%20of%20comprehensive%20banking%20services.pdf?dl=1"))
+        request.setTitle("services.pdf")
+        request.setDescription("Downloading")
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        request.setDestinationInExternalPublicDir(
+            Environment.DIRECTORY_DOWNLOADS,
+            "services.pdf"
+        )
+        val manager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        manager.enqueue(request)
+
 
         binding.apply {
-            pdfViewer.fromAsset("Kotlin.pdf").load()
+
+            val file = File(Environment.DIRECTORY_DOWNLOADS + "services.pdf")
+            pdfViewer.fromFile(file.path)
+                //.enableSwipe(true)
+                //.enableDoubletap(true)
+                //.enableAnnotationRendering(false)
+                .load()
+
+            //pdfViewer.fromAsset("Kotlin.pdf").load()
         }
 
 
